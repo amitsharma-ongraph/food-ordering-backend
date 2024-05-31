bbb3e8451a87bb8a0b3370975001fc2a85341920
jsdeveloper <js.developer@ongraph.com>
2024-05-31 17:44:19 +0530

    same site error

--- src/index.ts
+++ src/index.ts
@@ -24,7 +24,6 @@ app.use(
     keys: [process.env.SESSION_SECRET || ""],
     maxAge: 24 * 60 * 60 * 1000,
     secure: process.env.NODE_ENV === "production",
-    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
   })
 );
 
