f999d6c15a7de1c4e9d4928387807f344e059fbf
jsdeveloper <js.developer@ongraph.com>
2024-06-05 14:56:51 +0530

    disabling security for testing

--- src/index.ts
+++ src/index.ts
@@ -22,8 +22,8 @@ app.use(
     name: "session",
     keys: [process.env.SESSION_SECRET || ""],
     maxAge: 24 * 60 * 60 * 1000,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    secure: false,
+    sameSite:"none"
   })
 );
 
