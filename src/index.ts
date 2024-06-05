fe520414d539f3e0944204cd15a65208f7a4b6f9
jsdeveloper <js.developer@ongraph.com>
2024-06-05 15:11:36 +0530

    secured

--- src/index.ts
+++ src/index.ts
@@ -22,7 +22,7 @@ app.use(
     name: "session",
     keys: [process.env.SESSION_SECRET || ""],
     maxAge: 24 * 60 * 60 * 1000,
-    secure: process.env.NODE_ENV === "production",
+    secure: process.env.NODE_ENV === "production", 
   })
 );
 
