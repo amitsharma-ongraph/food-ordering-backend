db0693349889f21c4e22b879e12a2f22cbe3959e
jsdeveloper <js.developer@ongraph.com>
2024-06-20 11:54:01 +0530

    Node ENV 2

--- src/index.ts
+++ src/index.ts
@@ -25,6 +25,7 @@ app.use(
     maxAge: 24 * 60 * 60 * 1000,
     secure: process.env.NODE_ENV === "production",
     sameSite: process.env.Node_ENV === "production" ? "none" : "lax",
+    httpOnly: true,
   })
 );
 
