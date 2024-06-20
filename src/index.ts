b23043d8bf45c12f386a0651feb787f2ec5a3179
jsdeveloper <js.developer@ongraph.com>
2024-06-20 11:51:18 +0530

    NODE ENV

--- src/index.ts
+++ src/index.ts
@@ -23,8 +23,8 @@ app.use(
     name: "session",
     keys: [process.env.SESSION_SECRET || ""],
     maxAge: 24 * 60 * 60 * 1000,
-    secure: true,
-    sameSite: "none",
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.Node_ENV === "production" ? "none" : "lax",
   })
 );
 
